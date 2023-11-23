b11d250213f1573b5f1f4242a91605f3ccf4d518
Seiko Santana <santanaaseiko@gmail.com>
2023-11-23 15:18:00 +0700

    Add MMA8452Q support

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,17 @@
 // ESP8266 Guide: https://RandomNerdTutorials.com/esp8266-nodemcu-mpu-6050-accelerometer-gyroscope-arduino/
 // Arduino Guide: https://RandomNerdTutorials.com/arduino-mpu-6050-accelerometer-gyroscope/
 
+#include <Wire.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
-#include <Wire.h>
+#include <SparkFun_MMA8452Q.h>
+#include <memory>
+#include "sensor_modes.hpp"
 
-Adafruit_MPU6050 mpu;
+std::shared_ptr<Adafruit_MPU6050> mpu;
+std::shared_ptr<MMA8452Q> mma;
 
+SensorMode sensorMode;
 
 void waitSerialAvailable(void)
 {
@@ -21,7 +26,17 @@ void waitSerialAvailable(void)
 
 void userMpuConfig(void)
 {
-  Serial.println("config");
+  mpu = std::make_shared<Adafruit_MPU6050>();
+  if (!mpu->begin())
+  {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1)
+    {
+      delay(10);
+    }
+  }
+
+  sensorMode = SensorMode::MPU;
 
   char input;
 
@@ -29,76 +44,76 @@ void userMpuConfig(void)
   input = Serial.read();
   if (input == '1')
   {
-    mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
+    mpu->setAccelerometerRange(MPU6050_RANGE_2_G);
   }
   else if (input == '2')
   {
-    mpu.setAccelerometerRange(MPU6050_RANGE_4_G);
+    mpu->setAccelerometerRange(MPU6050_RANGE_4_G);
   }
   else if (input == '3')
   {
-    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+    mpu->setAccelerometerRange(MPU6050_RANGE_8_G);
   }
   else if (input == '4')
   {
-    mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
+    mpu->setAccelerometerRange(MPU6050_RANGE_16_G);
   }
 
   waitSerialAvailable();
   input = Serial.read();
   if (input == '1')
   {
-    mpu.setGyroRange(MPU6050_RANGE_250_DEG);
+    mpu->setGyroRange(MPU6050_RANGE_250_DEG);
   }
   else if (input == '2')
   {
-    mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+    mpu->setGyroRange(MPU6050_RANGE_500_DEG);
   }
   else if (input == '3')
   {
-    mpu.setGyroRange(MPU6050_RANGE_1000_DEG);
+    mpu->setGyroRange(MPU6050_RANGE_1000_DEG);
   }
   else if (input == '4')
   {
-    mpu.setGyroRange(MPU6050_RANGE_2000_DEG);
+    mpu->setGyroRange(MPU6050_RANGE_2000_DEG);
   }
 
   waitSerialAvailable();
   input = Serial.read();
   if (input == '1')
   {
-    mpu.setFilterBandwidth(MPU6050_BAND_260_HZ);
+    mpu->setFilterBandwidth(MPU6050_BAND_260_HZ);
   }
   else if (input == '2')
   {
-    mpu.setFilterBandwidth(MPU6050_BAND_184_HZ);
+    mpu->setFilterBandwidth(MPU6050_BAND_184_HZ);
   }
   else if (input == '3')
   {
-    mpu.setFilterBandwidth(MPU6050_BAND_94_HZ);
+    mpu->setFilterBandwidth(MPU6050_BAND_94_HZ);
   }
   else if (input == '4')
   {
-    mpu.setFilterBandwidth(MPU6050_BAND_44_HZ);
+    mpu->setFilterBandwidth(MPU6050_BAND_44_HZ);
   }
   else if (input == '5')
   {
-    mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+    mpu->setFilterBandwidth(MPU6050_BAND_21_HZ);
   }
   else if (input == '6')
   {
-    mpu.setFilterBandwidth(MPU6050_BAND_10_HZ);
+    mpu->setFilterBandwidth(MPU6050_BAND_10_HZ);
   }
   else if (input == '7')
   {
-    mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+    mpu->setFilterBandwidth(MPU6050_BAND_5_HZ);
   }
 }
 
 void printMpuStatus(void)
 {
   Serial.print("Accelerometer range set to: ");
-  switch (mpu.getAccelerometerRange())
+  switch (mpu->getAccelerometerRange())
   {
   case MPU6050_RANGE_2_G:
     Serial.println("+-2G");
@@ -115,7 +130,7 @@ void printMpuStatus(void)
   }
 
   Serial.print("Gyro range set to: ");
-  switch (mpu.getGyroRange())
+  switch (mpu->getGyroRange())
   {
   case MPU6050_RANGE_250_DEG:
     Serial.println("+- 250 deg/s");
@@ -132,7 +147,7 @@ void printMpuStatus(void)
   }
 
   Serial.print("Filter bandwidth set to: ");
-  switch (mpu.getFilterBandwidth())
+  switch (mpu->getFilterBandwidth())
   {
   case MPU6050_BAND_260_HZ:
     Serial.println("260 Hz");
@@ -158,6 +173,84 @@ void printMpuStatus(void)
   }
 }
 
+void userMMAConfig()
+{
+  mma = std::make_shared<MMA8452Q>();
+  Wire.begin();
+
+  while (mma->begin(Wire, 0x1C) == false)
+  {
+    Serial.println("Not Connected. Please check connections and read the hookup guide.");
+    delay(3000);
+  }
+
+  sensorMode = SensorMode::MMA;
+
+  char input;
+
+  waitSerialAvailable();
+  input = Serial.read();
+  if (input == '1')
+  {
+    mma->setScale(SCALE_2G);
+  }
+  else if (input == '2')
+  {
+    mma->setScale(SCALE_4G);
+  }
+  else if (input == '3')
+  {
+    mma->setScale(SCALE_8G);
+  }
+}
+
+void config()
+{
+  Serial.println("config");
+  char input;
+
+  waitSerialAvailable();
+  input = Serial.read();
+  if (input == '1')
+  {
+    userMMAConfig();
+    Serial.print("Timespan (ms)");
+    Serial.print(";");
+    Serial.print("Accel X (raw)");
+    Serial.print(";");
+    Serial.print("Accel Y (raw)");
+    Serial.print(";");
+    Serial.print("Accel Z (raw)");
+    Serial.print(";");
+    Serial.print("Accel X (g)");
+    Serial.print(";");
+    Serial.print("Accel Y (g)");
+    Serial.print(";");
+    Serial.print("Accel Z (g)");
+  }
+  else if (input == '2')
+  {
+    // mpu mode
+    userMpuConfig();
+    printMpuStatus();
+    Serial.print("Timespan (ms)");
+    Serial.print(";");
+    Serial.print("Accel X (m/s^2)");
+    Serial.print(";");
+    Serial.print("Accel Y (m/s^2)");
+    Serial.print(";");
+    Serial.print("Accel Z (m/s^2)");
+    Serial.print(";");
+    Serial.print("Gyro X (rad/s)");
+    Serial.print(";");
+    Serial.print("Gyro Y (rad/s)");
+    Serial.print(";");
+    Serial.print("Gyro Z (rad/s)");
+    Serial.print(";");
+    Serial.println("Temperature (degC)");
+  }
+}
+
 void setup(void)
 {
   Serial.begin(115200);
@@ -167,60 +260,57 @@ void setup(void)
   Serial.flush();
   Serial.println();
 
-  // Try to initialize!
-  if (!mpu.begin())
-  {
-    Serial.println("Failed to find MPU6050 chip");
-    while (1)
-    {
-      delay(10);
-    }
-  }
-
-  userMpuConfig();
-  printMpuStatus();
+  config();
 
   delay(100);
-  Serial.print("Timespan (ms)");
-  Serial.print(";");
-  Serial.print("Accel X (m/s^2)");
-  Serial.print(";");
-  Serial.print("Accel Y (m/s^2)");
-  Serial.print(";");
-  Serial.print("Accel Z (m/s^2)");
-  Serial.print(";");
-  Serial.print("Gyro X (rad/s)");
-  Serial.print(";");
-  Serial.print("Gyro Y (rad/s)");
-  Serial.print(";");
-  Serial.print("Gyro Z (rad/s)");
-  Serial.print(";");
-  Serial.println("Temperature (degC)");
 }
 
 void loop()
 {
+  if (sensorMode == SensorMode::MMA)
+  {
+    if (mma->available())
+    { // Wait for new data from accelerometer
+      // Acceleration of x, y, and z directions in g units
+      Serial.print(millis());
+      Serial.print(";");
+      Serial.print(mma->getX());
+      Serial.print(";");
+      Serial.print(mma->getY());
+      Serial.print(";");
+      Serial.print(mma->getZ());
+      Serial.print(";");
+      Serial.print(mma->getCalculatedX(), 7);
+      Serial.print(";");
+      Serial.print(mma->getCalculatedY(), 7);
+      Serial.print(";");
+      Serial.print(mma->getCalculatedZ(), 7);
+      Serial.println();
+    }
+  }
+  else if (sensorMode == SensorMode::MPU)
+  {
+    /* Get new sensor events with the readings */
+    sensors_event_t a, g, temp;
+    mpu->getEvent(&a, &g, &temp);
 
-  /* Get new sensor events with the readings */
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
+    /* Print out the values */
+    Serial.print(millis());
+    Serial.print(";");
+    Serial.print(a.acceleration.x);
+    Serial.print(";");
+    Serial.print(a.acceleration.y);
+    Serial.print(";");
+    Serial.print(a.acceleration.z);
+    Serial.print(";");
 
-  /* Print out the values */
-  Serial.print(millis());
-  Serial.print(";");
-  Serial.print(a.acceleration.x);
-  Serial.print(";");
-  Serial.print(a.acceleration.y);
-  Serial.print(";");
-  Serial.print(a.acceleration.z);
-  Serial.print(";");
+    Serial.print(g.gyro.x);
+    Serial.print(";");
+    Serial.print(g.gyro.y);
+    Serial.print(";");
+    Serial.print(g.gyro.z);
 
-  Serial.print(g.gyro.x);
-  Serial.print(";");
-  Serial.print(g.gyro.y);
-  Serial.print(";");
-  Serial.print(g.gyro.z);
-
-  Serial.print(";");
-  Serial.println(temp.temperature);
+    Serial.print(";");
+    Serial.println(temp.temperature);
+  }
 }
---
+++ src/sensor_modes.hpp
@@ -0,0 +1,10 @@
+#ifndef SENSOR_MODES_HPP
+#define SENSOR_MODES_HPP
+
+typedef enum SensorMode
+{
+    MMA,
+    MPU
+};
+
+#endif
